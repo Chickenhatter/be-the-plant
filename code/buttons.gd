f50acbc35f62d771349ab51f0af04351fc22b4d4
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-08-12 21:12:20 +1200

    9hr 30m adding text area and some text

--- code/buttons.gd
+++ code/buttons.gd
@@ -12,8 +12,41 @@ func _process(delta: float) -> void:
 
 
 func _on_glycolysis_mouse_entered() -> void:
+	$glycolysis/RichTextLabel.text = 'This turns off and on, when turned on it turns water and carbon dioxide into glucose but the rate of this is affected by weather'
+
+func _on_glycolysis_mouse_exited() -> void:
+	$glycolysis/RichTextLabel.text = ''
+
+
+func _on_stomata_mouse_entered() -> void:
+	$glycolysis/RichTextLabel.text = 'This turns off and on, when turned on it turns water into carbon dioxide'
+
+
+func _on_stomata_mouse_exited() -> void:
+	$glycolysis/RichTextLabel.text = ''
+
+
+func _on_mushroom_mouse_entered() -> void:
+	$mushroom/RichTextLabel.text = 'This is able to be moved by your mouse when holding left click, it will decrease glucose but will increase nitrogen'
+	$mushroom/RichTextLabel2.text = 'It spawns this stuff on the trees'
+
+
+func _on_mushroom_mouse_exited() -> void:
+	$mushroom/RichTextLabel.text = ''
+	$mushroom/RichTextLabel2.text = ''
+
+
+func _on_chloroplast_mouse_entered() -> void:
 	pass # Replace with function body.
 
 
-func _on_glycolysis_mouse_exited() -> void:
+func _on_chloroplast_mouse_exited() -> void:
+	pass # Replace with function body.
+
+
+func _on_ttube_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_ttube_mouse_exited() -> void:
 	pass # Replace with function body.
