ac1c696c202b6b4bd1166cc83cafb0e8c8081e4a
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-08-15 23:13:19 +1200

    10hr 3m text added for tubes chloroplast sluder and weather

--- code/buttons.gd
+++ code/buttons.gd
@@ -37,16 +37,35 @@ func _on_mushroom_mouse_exited() -> void:
 
 
 func _on_chloroplast_mouse_entered() -> void:
-	pass # Replace with function body.
-
+	$chloroplast/RichTextLabel.text = 'This is a slider which increases the leaf size and will cause glucose production when the button is on to increase'
+	$chloroplast/RichTextLabel2.text = 'The leaf size should increase'
 
 func _on_chloroplast_mouse_exited() -> void:
-	pass # Replace with function body.
+	$chloroplast/RichTextLabel.text = ''
+	$chloroplast/RichTextLabel2.text = ''
 
 
 func _on_ttube_mouse_entered() -> void:
-	pass # Replace with function body.
+	$ttube/RichTextLabel.text = 'amount of nitrogen'
+	$ttube/RichTextLabel2.text = 'amount of carbon dioxide'
+	$ttube/RichTextLabel3.text = 'amount of water'
+	$ttube/RichTextLabel4.text = 'amount of glucose'
+	$ttube/RichTextLabel5.text = 'These all tell how much of each type of resource is left, if you run out of glucose the game ends '
 
 
 func _on_ttube_mouse_exited() -> void:
-	pass # Replace with function body.
+	$ttube/RichTextLabel.text = ''
+	$ttube/RichTextLabel2.text = ''
+	$ttube/RichTextLabel3.text = ''
+	$ttube/RichTextLabel4.text = ''
+	$ttube/RichTextLabel5.text = ''
+
+func _on_weather_mouse_entered() -> void:
+	$weather/RichTextLabel.text = 'Weather affects the water percent in the soil, this percentage will passivly increase the water bar/tube'
+	$weather/RichTextLabel2.text = 'Weather can be seen by clouds rain or just a sunny sky like right now'
+	$weather/RichTextLabel3.text = 'There is a day and night cycle during night photosynthesis dose not work and during day it dose work and produce glucose'
+
+func _on_weather_mouse_exited() -> void:
+	$weather/RichTextLabel.text = ''
+	$weather/RichTextLabel2.text = ''
+	$weather/RichTextLabel3.text = ''
